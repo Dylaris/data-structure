d984a5a41b8f463508347ff171aeeeacfccb05de
Dylaris <3445818679@qq.com>
2025-03-19 22:48:58 +0800

    add .gitignore

--- hash/main.c
+++ hash/main.c
@@ -13,20 +13,20 @@ int main(void)
     hash_insert(&ht, "ggg", 7);
     hash_insert(&ht, "eee", 8);
 
-    printf("<key: aaa, value: %d\n", hash_search(ht, "aaa"));
-    printf("<key: ccc, value: %d\n", hash_search(ht, "ccc"));
-    printf("<key: xxx, value: %d\n", hash_search(ht, "xxx"));
-    printf("<key: fff, value: %d\n", hash_search(ht, "fff"));
-    printf("<key: eee, value: %d\n", hash_search(ht, "eee"));
+    printf("<key: aaa, value: %2d>\n", hash_search(ht, "aaa"));
+    printf("<key: ccc, value: %2d>\n", hash_search(ht, "ccc"));
+    printf("<key: xxx, value: %2d>\n", hash_search(ht, "xxx"));
+    printf("<key: fff, value: %2d>\n", hash_search(ht, "fff"));
+    printf("<key: eee, value: %2d>\n", hash_search(ht, "eee"));
 
     hash_delete(&ht, "aaa");
     hash_delete(&ht, "bbb");
     hash_delete(&ht, "ggg");
     hash_delete(&ht, "eee");
 
-    printf("<key: aaa, value: %d\n", hash_search(ht, "aaa"));
-    printf("<key: ccc, value: %d\n", hash_search(ht, "ccc"));
-    printf("<key: eee, value: %d\n", hash_search(ht, "eee"));
+    printf("<key: aaa, value: %2d>\n", hash_search(ht, "aaa"));
+    printf("<key: ccc, value: %2d>\n", hash_search(ht, "ccc"));
+    printf("<key: eee, value: %2d>\n", hash_search(ht, "eee"));
 
     return 0;
 }
